660fc9d9032be46bdfd3dd256f54ed3ef8e1b277
Tom C. Adler <tocto@mailbox.org>
2021-02-06 00:14:43 +0100

    added first (empty) test body

---
+++ UnitTests/Model/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Model
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Calc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
