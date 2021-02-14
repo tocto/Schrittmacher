2c5d930219f7119faebaf7c54a872c0fde2b57b0
Tom C. Adler <tocto@mailbox.org>
2021-02-14 22:55:48 +0100

    fixed build bug (livechart nuget makes trouble)

--- Phileas/Views/UserControls/MathModelView.xaml.cs
+++ Phileas/Views/UserControls/MathModelView.xaml.cs
@@ -37,5 +37,15 @@ namespace Phileas.Views.UserControls
             var expression = (sender as FrameworkElement).DataContext as MathModelExpression;
             if (expression != null) this.MathModel.Expressions.Remove(expression);
         }
+
+        private void TextBox_MathModelExpressions_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_MathModelExpressions_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
