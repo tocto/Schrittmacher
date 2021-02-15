0816b5d8fa84f1841691ac88fffc73990a8e4ee6
Tom C. Adler <tocto@mailbox.org>
2021-02-15 14:57:41 +0100

    added class PlotData

---
+++ Phileas/Model/PlotData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phileas.Model
+{
+    public class PlotData : INotifyPropertyChanged
+    {
+        private string title = string.Empty;
+
+        private string xAxisTitle = string.Empty;
+        
+        private string yAxisTitle = string.Empty;
+
+        private Dictionary<string, List<double>> dataPoints = new Dictionary<string, List<double>>();
+
+        public string Title
+        {
+            get => this.title;
+
+            set
+            {
+                if (this.title != value)
+                {
+                    this.title = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
+                }
+            }
+        }
+
+        public string XAxisTitle
+        {
+            get => this.xAxisTitle;
+
+            set
+            {
+                if (this.xAxisTitle != value)
+                {
+                    this.xAxisTitle = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("XAxisTitle"));
+                }
+            }
+        }
+
+        public string YAxisTitle
+        {
+            get => this.yAxisTitle;
+
+            set
+            {
+                if (this.yAxisTitle != value)
+                {
+                    this.yAxisTitle = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("YAxisTitle"));
+                }
+            }
+        }
+
+
+        public Dictionary<string, List<double>> DataPoints
+        {
+            get => this.dataPoints;
+
+            set
+            {
+                if (this.dataPoints != value)
+                {
+                    this.dataPoints = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DataPoints"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
