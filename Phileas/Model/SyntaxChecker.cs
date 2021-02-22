332bf353ae61d3189809a6318acf7f0f4a26a26f
Tom C. Adler <tocto@mailbox.org>
2021-02-22 15:02:30 +0100

    basic syntax checker established for debug mode

---
+++ Phileas/Model/SyntaxChecker.cs
@@ -0,0 +1,32 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schrittmacher.Model
+{
+    public class SyntaxChecker
+    {
+
+        public bool Check(string expressionString)
+        {
+            bool isValid = true;
+
+            MathModelExpression mme = new MathModelExpression(expressionString);
+            if (mme.MathText.Length == 0) return true; // no math string to validate
+
+            if (mme.MathText != string.Empty)
+            {
+                Expression expression = new Expression(expressionString);
+                isValid = expression.checkLexSyntax();
+
+                Debug.WriteLine(expression.getErrorMessage());
+            }
+
+            return isValid;
+        }
+    }
+}
--- Phileas/Views/UserControls/MathModelView.xaml.cs
+++ Phileas/Views/UserControls/MathModelView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Schrittmacher.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -20,9 +21,30 @@ namespace Schrittmacher.Views.UserControls
 {
     public sealed partial class MathModelView : UserControl
     {
+        SyntaxChecker syntaxChecker = new SyntaxChecker();
+
         public MathModelView()
         {
             this.InitializeComponent();
+        }
+
+        private void TextBox_MathModelExpressions_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBox_MathModelExpressions.Text != null 
+                && TextBox_MathModelExpressions.Text.Trim().Length > 0 
+                &&  TextBox_MathModelExpressions.Text.Last().Equals('\r'))
+            {
+                using (StringReader reader = new StringReader(TextBox_MathModelExpressions.Text))
+                {
+                    string[] lines = TextBox_MathModelExpressions.Text.Split('\r');
+
+                    foreach (var line in lines)
+                    {
+                        Debug.WriteLine("Syntax: " + syntaxChecker.Check(line));
+                    }
+                }
+            }
+            
         }
     }
 }
